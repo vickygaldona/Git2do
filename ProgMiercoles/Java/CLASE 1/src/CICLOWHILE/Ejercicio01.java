5f31800e46e4a5854087e7fc497d571f531c37c0
Victoria <vickygaldona@gmail.com>
2024-08-16 16:28:01 -0300

    while, do while y for

---
+++ ProgMiercoles/Java/CLASE 1/src/CICLOWHILE/Ejercicio01.java
@@ -0,0 +1,21 @@
+package CICLOWHILE;
+
+public class Ejercicio01 {
+    public static void main(String[] args) {
+        var conteo = 0; // inferencia de tipos
+        while (conteo < 7) {
+            System.out.println("conteo = " + conteo);
+            conteo++; // vamos aumentando en uno la variable
+        }
+
+        var contador = 0;
+        do {
+            System.out.println("contador = " + contador);
+            contador++;
+        } while (contador < 7);
+
+        for (var contando = 0; contando < 7; contando++) {
+            System.out.println("contando = " + contando);
+        }
+    }
+}

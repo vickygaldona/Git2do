ea184ba84f7543478afa54c69886d627e3c0346f
Victoria Galdona <vickygaldona@gmail.com>
2024-11-02 12:50:17 -0300

    clase 10 de python párte 2
    terminamos clase 10 de python y empezamos parte de la 11 con diseño de clase

--- ProgLunes/Python/clase10/MiClase.py
+++ ProgLunes/Python/clase10/MiClase.py
@@ -1,5 +1,3 @@
-from tkinter.constants import PROJECTING
-
 
 class MiClase:
     # VARIABLE DE CLASE, ESTE ATRIBUTO DARA A CADA OBJETO EL MISMO VALOR
@@ -8,6 +6,19 @@ class MiClase:
     def __init__(self, variable_instancia): #LA VARIABLE DE INSTANCIA, DE DIFERENTES VALORES
         self.variable_instancia = variable_instancia
 
+    @staticmethod
+    def metodo_estatico():
+        print(MiClase.variable_clase)
+
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase)
+
+    def metodo_instancia(self):
+        self.metodo_clase()
+        self.metodo_estatico()
+        print(self.variable_clase)
+        print(self.variable_instancia)
 
 print(MiClase.variable_clase)
 miClase1 = MiClase('Esta es una variable de instancia')
@@ -16,3 +27,16 @@ print(miClase1.variable_clase)
 miClase2= MiClase('Esta es otra prueba de variable de instancia')
 print(miClase2.variable_instancia)
 print(miClase2.variable_clase)
+
+MiClase.variable_clase2 = 'Valor de variable de clase 2'
+print(MiClase.variable_clase2)
+print(miClase1.variable_clase2)
+print(miClase2.variable_clase2)
+
+MiClase.metodo_estatico()
+
+
+MiClase.metodo_clase()
+miObjeto1 = MiClase('variable de instancia')
+miObjeto1.metodo_clase()
+miObjeto1.metodo_instancia()
